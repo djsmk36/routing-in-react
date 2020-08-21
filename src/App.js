fe1b6e37d29552585535d2ee3522ccc8ebdbcf32
Edward Valdivia Jr <evvaldiv@gmail.com>
2020-08-21 16:13:08 -0500

    homework tweak

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/welcome/Welcome';
+import Clock from './components/clock/Clock'
+import Contact from './components/contact/Contact'
+import Page404 from './components/page404/Page404'
+import Navigation from './components/navigation/Navigation'
+import { Route, BrowserRouter } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+        <Route
+          path='/welcome/:name'
+          render={props => (
+            <Welcome {...props} name={props.match.params.name} />
+          )}
+        />
+        <Route
+          exact
+          path='/'
+          render={(props) => <Welcome {...props} name='Test' />}
+        />
+        <Route path='/clock' component={Clock} />
+        <Route path='/contact' component={Contact} />
+        <Route component={Page404} />
+      </Switch>
     </div>
   );
 }
